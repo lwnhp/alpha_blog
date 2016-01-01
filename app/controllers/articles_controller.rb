01f192239191b27ed6b47d67fa8aca6ae395d05b
Berry Straver <bms@magistraver.nl>
2016-01-01 19:03:21 +0000

    add association article user / add debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,7 +36,9 @@ class ArticlesController < ApplicationController
    end
    
    def create
+    # debugger
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
      flash[:success] = "Artikel succesvol aangemaakt"
      redirect_to article_path(@article)
