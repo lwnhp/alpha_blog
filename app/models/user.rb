b03c5a0ab7e40dd7f946bf4953999cae665e4872
Berry Straver <bms@magistraver.nl>
2016-01-01 15:43:56 +0000

    lost connection?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
  validates :username, presence: true,
-            uniqueness: { case_sensitive: false },
-            lenght: { in: 3..5 }
- VALID_EMAIL_REGEX: 
+            uniqueness: { case_sensitive: false }
+            # lenght: { in: 3..5 }
+
  validates :email, presence: true,
-            uniqueness: { case_sensitive: false },
-            lenght: { maximum: 105 }
-            format: { with: VALID_EMAIL_REGEX }
+            uniqueness: { case_sensitive: false }
+            # lenght: { maximum: 105 }
+ validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
