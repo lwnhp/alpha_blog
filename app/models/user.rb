0980b217acd128b381205a7c39d69aab9a3cf28d
Berry Straver <bms@magistraver.nl>
2016-01-01 17:45:12 +0000

    user attribute validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
  validates :username, presence: true,
-            uniqueness: { case_sensitive: false }
-            # lenght: { in: 3..5 }
+            uniqueness: { case_sensitive: false },
+            length: { in: 3..5 }
 
+ VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
  validates :email, presence: true,
-            uniqueness: { case_sensitive: false }
-            # lenght: { maximum: 105 }
- validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
+             
+ validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create # doet hetzelfde als REGEX
 end
